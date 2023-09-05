f6022e01e6ae0e27a31abb412dd5957f46f5e9fe
Ziyuan Wen <wenziyuan2000@126.com>
2023-09-05 14:21:27 -0500

    task5

---
+++ mat_to_numpy.py
@@ -0,0 +1,4 @@
+import numpy as np
+from scipy import io, integrate, linalg, signal
+from scipy.sparse.linalg import cg, eigs
+
---
+++ plot_mat.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot([1,2,3,4], [1,2,7,14])
+plt.axis([0, 6, 0, 20])
+plt.show()
+
+# x=np.arange(20)
+# y=np.cos(x*np.pi/10)
+# plt.plot(x,y)
+# plt.show()
